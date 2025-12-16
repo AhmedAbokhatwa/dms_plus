50274f909b775c219ada118171d25f1782e2d4a7
AhmedAbukhatwa <Ahmedabukhatwa1@gmail.com>
2025-12-16 14:08:42 +0200

    chore: comment out Company form query experiment

--- dms_plus/public/js/company.js
+++ dms_plus/public/js/company.js
@@ -1,13 +1,13 @@
-frappe.ui.form.on('Company', {
-	setup(frm) {
-	frm.set_query("default_in_transit_warehouse", function () {
-			return {
-				filters: {
-					warehouse_type: "Shipment",
-					is_group: 0,
-					company: frm.doc.company_name,
-				},
-			};
-		});
-	},
-})
+// frappe.ui.form.on('Company', {
+// 	setup(frm) {
+// 	frm.set_query("default_in_transit_warehouse", function () {
+// 			return {
+// 				filters: {
+// 					warehouse_type: "Shipment",
+// 					is_group: 0,
+// 					company: frm.doc.company_name,
+// 				},
+// 			};
+// 		});
+// 	},
+// })
