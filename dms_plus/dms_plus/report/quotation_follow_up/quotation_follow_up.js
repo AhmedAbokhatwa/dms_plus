f9cee8dc38fd809730a61beb691bd147b03ce36b
AhmedAbukhatwa <Ahmedabukhatwa1@gmail.com>
2026-01-04 10:49:10 +0200

    feat: New Report Quotation Follow Up

---
+++ dms_plus/dms_plus/report/quotation_follow_up/quotation_follow_up.js
@@ -0,0 +1,85 @@
+// Copyright (c) 2025, dms developers and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Quotation Follow-up"] = {
+	   "filters": [
+        {
+            "fieldname": "quotation_name",
+            "label": "Quotation Name",
+            "fieldtype": "Link",
+            "options": "Quotation",
+            "width": 180
+        },
+        {
+            "fieldname": "quotation_status",
+            "label": "Quotation Status",
+            "fieldtype": "Select",
+            "options": "",
+            "default": "",
+            "width": 120
+        },
+        {
+            "fieldname": "from_date",
+            "label": "From Date",
+            "fieldtype": "Date",
+            "width": 100
+        },
+        {
+            "fieldname": "to_date",
+            "label": "To Date",
+            "fieldtype": "Date",
+            "width": 100
+        },
+        {
+            "fieldname": "item",
+            "label": "Item",
+            "fieldtype": "Link",
+            "options": "Item",
+            "width": 120
+        },
+        {
+            "fieldname": "item_group",
+            "label": "Item Group",
+            "fieldtype": "Link",
+            "options": "Item Group",
+            "width": 120
+        },
+        {
+            "fieldname": "customer_name",
+            "label": "Customer",
+            "fieldtype": "Link",
+            "options": "Customer",
+            "width": 150
+        },
+        {
+            "fieldname": "warehouse",
+            "label": "Warehouse",
+            "fieldtype": "Link",
+            "options": "Warehouse",
+            "width": 150
+        },
+        {
+            "fieldname": "account_manager",
+            "label": "Account Manager",
+            "fieldtype": "Link",
+            "options": "Employee",
+            "width": 180
+        },
+    ],
+     onload: function (report) {
+        frappe.model.with_doctype("Quotation", function () {
+            const meta = frappe.get_meta("Quotation");
+            const status_field = meta.fields.find(
+                f => f.fieldname === "status"
+            );
+
+            if (status_field) {
+                const options = ["", ...status_field.options.split("\n")];
+
+                report.set_filter_value("quotation_status", "");
+                report.get_filter("quotation_status").df.options = options.join("\n");
+                report.get_filter("quotation_status").refresh();
+            }
+        });
+    },
+};
